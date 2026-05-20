722a17df7ad6e6473dd38364c4aef20c68285660
dyrnq <dyrnq@qq.com>
2026-05-20 10:09:46 +0800

    chore: project scaffold

---
+++ build.rs
@@ -0,0 +1,20 @@
+use std::process::Command;
+
+fn main() {
+    // Git short commit hash
+    let git_hash = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .output()
+        .ok()
+        .and_then(|o| String::from_utf8(o.stdout).ok())
+        .map(|s| s.trim().to_string())
+        .unwrap_or_else(|| "unknown".to_string());
+
+    // Build date (UTC)
+    let build_date = chrono::Utc::now().format("%Y-%m-%d").to_string();
+
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    println!("cargo:rustc-env=BUILD_DATE={}", build_date);
+    println!("cargo:rustc-env=TARGET_TRIPLE={}", std::env::var("TARGET").unwrap_or_else(|_| "unknown".to_string()));
+    println!("cargo:rerun-if-changed=.git/HEAD");
+}
